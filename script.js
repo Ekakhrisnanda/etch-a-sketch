83e1274b314a108810689ddfece88ccb6c017e4b
Eduk <ekakhrisnanda@gmail.com>
2023-01-11 22:32:28 +0700

    Create black color button

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const btnsContainer = document.querySelector('.color-buttons');
 const board = function(cols, rows) {
     for (let i = 0; i < (cols * rows); i++) {
         const div = document.createElement('div');
-        div.style.border = '1px solid black'
-        container.style.gridTemplateColumns = 'repeat(${cols}, 1fr)';
-        container.style.gridTemplateRows = 'repeat(${rows}, 1fr)';
+        div.style.border = '1px solid blue';
+        container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('grid');
     }
 }
@@ -18,40 +18,15 @@ board(16, 16);
 
 const blackColor = function() {
     const grids = container.querySelectorAll('.grid');
-    btnBlack.textContent = 'Black'
+    btnBlack.textContent = 'Black';
     btnBlack.addEventListener('click', () => {
-        grids.forEach(grid => grid.addEventListener('mouseover', () => {
-            let colorCode = Math.floor(Math.random() * 255) + 1;
-            grid.style.background = 'rgb(${colorCode}, ${colorCode}, ${colorCode})';
+        grids.forEach((grid) => grid.addEventListener('mouseover', () => {
+            grid.style.background = 'black';
         }))
 
     })
 
-    btnsContainer.appendChild(btnBlack);
+    btnsContainer.appendChild(btnBlack).classList.add('btn');
 }
 
-// const rgbColor =  function() {
-//     const grids = container.querySelectorAll('.grid');
-//     btnBlack.textContent = 'RGB';
-//     btnBlack.addEventListener('click', () => {
-//         grids.forEach(grid => grid.addEventListener('mouseover', () => {
-//             grid.style.background =
-//         }))
-
-//     })
-
-//     btnsContainer.appendChild(btnRgb);
-// }
-
-const eraser =  function() {
-    const grids = container.querySelectorAll('.grid');
-    btnBlack.textContent = 'Eraser';
-    btnBlack.addEventListener('click', () => {
-        grids.forEach(grid => grid.addEventListener('mouseover', () => {
-            grid.style.background = 'white'
-        }))
-
-    })
-
-    btnsContainer.appendChild(btnEraser);
-}
+blackColor();
