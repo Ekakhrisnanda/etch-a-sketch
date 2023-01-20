04594188787d58e8acfcc611cff91e102b628d0e
Eduk <ekakhrisnanda@gmail.com>
2023-01-20 23:05:37 +0700

    Fix color buttons in JS

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const container = document.querySelector('.board-container');
 const btnBlack = document.createElement('button');
 const btnEraser = document.createElement('button');
 const btnRgb = document.createElement('button');
+const btnRezise = document.createElement('button');
 const btnsContainer = document.querySelector('.color-buttons');
+let columns = 0;
+let rows = 0;
 
 const board = function(cols, rows) {
     for (let i = 0; i < (cols * rows); i++) {
         const div = document.createElement('div');
-        div.style.border = '1px solid blue';
+        div.style.border = '1px solid black';
         container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('grid');
@@ -16,11 +19,37 @@ const board = function(cols, rows) {
 
 board(16, 16);
 
+// const resizeButton = function() {
+//     const btn = container.querySelector('.resize-button');
+//     btnRezise.textContent = 'Resize';
+//     btnRezise.addEventListener('click', () => {
+//         rows = alert('Insert number of ROW!');
+//         columns = alert('Insert number of COLUMN!');
+//         // validate the input is number
+//         if (!typeof rows === 'number' && !typeof columns === 'number') {
+//             return alert('Insert number please!');
+//         } else {
+//             return true;
+//         }
+//         // set the limit
+//         if (columns > 100 && < 1 && rows > 100 && < 1 ) {
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     })
+
+// }
+
+// resizeButton()
+
+// color buttons
+
 const blackColor = function() {
     const grids = container.querySelectorAll('.grid');
     btnBlack.textContent = 'Black';
     btnBlack.addEventListener('click', () => {
-        grids.forEach((grid) => grid.addEventListener('mouseover', () => {
+        grids.forEach(grid => grid.addEventListener('mouseover', () => {
             grid.style.background = 'black';
         }))
 
@@ -30,3 +59,36 @@ const blackColor = function() {
 }
 
 blackColor();
+
+const eraser = function() {
+    const grids = container.querySelectorAll('.grid');
+    btnEraser.textContent = 'Eraser';
+    btnEraser.addEventListener('click', () => {
+        grids.forEach(grid => grid.addEventListener('mouseover', () => {
+            grid.style.background = 'white';
+        }))
+
+    })
+
+    btnsContainer.appendChild(btnEraser).classList.add('btn');
+}
+
+eraser();
+
+const rgbColor = function() {
+    const grids = container.querySelectorAll('.grid');
+    btnRgb.textContent = 'RGB';
+    btnRgb.addEventListener('click', () => {
+        grids.forEach((grid) => grid.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 256);
+            let G = Math.floor(Math.random() * 256);
+            let B = Math.floor(Math.random() * 256);
+            grid.style.background = `rgb(${R},${G},${B})`;
+        }))
+
+    })
+
+    btnsContainer.appendChild(btnRgb).classList.add('btn');
+}
+
+rgbColor();
